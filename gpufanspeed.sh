8c9469f16e66e90cb9d7e8f1248a709c256b307a
keijo <95128521+keijo0@users.noreply.github.com>
2022-02-26 03:52:51 +0200

    Add files via upload

--- gpufanspeed.sh
+++ gpufanspeed.sh
@@ -1,2 +1,2 @@
-#/bin/sh
-	nvidia-settings -a [gpu:0]/GPUFanControlState=1 && nvidia-settings -a [fan:0]/GPUTargetFanSpeed=28
+#/bin/sh 
+	nvidia-settings -a [gpu:0]/GPUFanControlState=1 && nvidia-settings -a [fan:0]/GPUTargetFanSpeed=35
