1cd478d4e81e08d8a676433f2c6f4efe3eaf91f0
idiot <95128521+keijo0@users.noreply.github.com>
2022-07-24 17:49:21 +0000

    Update bg.sh

--- bg.sh
+++ bg.sh
@@ -1,5 +1,1 @@
-#/bin/bash
-#feh --bg-fill --no-fehbg ~/Pictures/cas.jpg
-#feh --bg-fill --no-fehbg ~/Pictures/105189.jpg
-#feh --bg-fill --no-fehbg ~/Pictures/25573.jpg
-feh --bg-fill --no-fehbg ~/Pictures/105141.jpg
+feh --bg-fill --no-fehbg ~/Pictures/pic.jpg
