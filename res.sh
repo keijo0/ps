adcd91581e2e4a662a5344cb3a2793ceb5ae753d
idiot <95128521+keijo0@users.noreply.github.com>
2022-07-25 02:48:28 +0000

    Delete res.sh

--- res.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dbus-launch --sh-syntaxt --exit-with-session; xrandr --output DP-0 --mode 2560x1440 --rate 165.00 ; for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done 
