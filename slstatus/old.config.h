50c3ff7678526a280560d9f5769d32d85b0068d5
keijo0 <95128521+keijo0@users.noreply.github.com>
2022-09-19 11:15:41 +0300

    Add files via upload

--- slstatus/old.config.h
+++ slstatus/old.config.h
@@ -4,16 +4,20 @@ static const char unknown_str[] = "n/a";
 
 static const struct arg args[] = {
 //    	{ netspeed_rx, ": %sB/s | ",  "eno1" },
-//      { netspeed_tx, " : %sB/s | ",  "eno1" },
-//	{ run_command, "%s | ", "acpi" },
+//        { netspeed_tx, " : %sB/s | ",  "eno1" },
 	{ run_command, " %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ cpu_perc, " CPU %s%% | ", NULL	},
 	{ ram_perc, " RAM %s%% | ", NULL	},
 	{ disk_perc, " ROOT %s%% | ", "/" },
-	{ disk_perc, " HDDS (%s%% /", "/games" },
+        { disk_perc, " HDDS (%s%% /", "/games" },
 	{ disk_perc, " %s%%) | ", "/mnt/shit/games" },
 	{ run_command, " PACKAGES %s | ", "qlist -I | wc -l" },
 	{ uptime,   " UPTIME %s | ", NULL },
 	{ kernel_release, "KERNEL %s | ", NULL },
 	{ datetime, 	" %s",    "%B %d %a %T" },
 };
+
+/*static const struct arg args[] = {
+        { run_command, ":%4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ datetime, 	"%s",    "%b %d %a %T" },
+};*/
